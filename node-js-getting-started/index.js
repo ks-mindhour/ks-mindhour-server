56051d58064faab900e5f7a748de5cb1c6281d15
uzumaki-narut0 <kumar.swapnil170@gmail.com>
2017-04-04 22:30:21 +0530

    mongoose added

--- node-js-getting-started/index.js
+++ node-js-getting-started/index.js
@@ -4,13 +4,44 @@ var morgan = require('morgan');	//middleware
 //var hostname = 'localhost';
 
 // process.env.PORT lets the port be set by Heroku
-var portnumber = process.env.PORT || 8080;;
+var portnumber = process.env.PORT || 8080;
 
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.use(morgan('dev'));
 
-app.use(express.static(__dirname + '/public'));
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema,
+//ObjectId = Schema.ObjectId;
+var customer = new Schema({
+    firstname    		: String,
+    lastname     		: String,
+    contactnumber       : String,
+    password	        : String
+});
+
+Cust = mongoose.model('Cust', Schema);
+
+mongoose.connect(process.env.MONGOLAB_URI, function (error) {
+    if (error) console.error(error);
+    else console.log('mongo connected');
+});
+
+
+app.post('/', function(req, res) {
+    var f_name = req.body.first_name;
+    var l_name = req.body.last_name;
+    var mail_id = req.body.email;
+    var password = req.body.password;
+
+    res.send(f_name + ' ' + l_name + ' ' + mail_id);
+});
+
+
+//app.use(express.static(__dirname + '/public'));
 
 app.listen(portnumber,function(){
 	console.log('server is running at http://');
